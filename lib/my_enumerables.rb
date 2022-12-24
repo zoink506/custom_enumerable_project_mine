4a5eb32424617af5f6e545a1439a548a69e82a91
zoink506 <benhubble03@gmail.com>
2022-12-24 18:23:10 +1100

    Write my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -27,6 +27,28 @@ class Array
 
     return self
   end
+
+  def my_select
+    return_array = Array.new
+    i = 0
+    while i < self.length
+      return_array << self[i] if yield(self[i])
+      i += 1
+    end
+    return return_array
+  end
+
+  def my_all?
+    i = 0
+    while i < self.length
+      return false if !yield(self[i])
+      i += 1
+    end
+
+    return true
+  end
+
 end
 
-p [1, 2, 3].my_each_with_index { |value, index| puts "value: #{value}, index: #{index}" }
+p [1, 2, 3, 4].my_all? { |value| value < 5 }
+
